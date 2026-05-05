c497f619e2729f10713bee9826dc50edd41c6e71
comendrive-a11y <comendrive@gmail.com>
2026-05-05 22:30:53 +0300

    Create head.tsx

---
+++ app/quad-bike-fethiye/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>ATV Safari Fethiye | Quad Bike Ölüdeniz & Hisarönü</title>
+
+      <meta
+        name="description"
+        content="ATV safari in Fethiye, Ölüdeniz and Hisarönü. Quad bike tours, off-road experience and buggy safari alternative with longer routes and beach stops."
+      />
+    </>
+  );
+}
